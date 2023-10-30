2de3182aa3790469ef43ffa427ee562c99142a40
efmorales <54375116+efmorales@users.noreply.github.com>
2023-10-30 00:08:06 -0400

    fixed bugs by adding types to functions

--- main.ts
+++ main.ts
@@ -9,11 +9,11 @@ export default class StoryAggregatorPlugin extends Plugin {
   async handleFileChange(file: TFile) {
     const today = moment().format("YYYY-MM-DD");
     const dailyNotePath = today + '.md';
-  
+
     if (file.path !== dailyNotePath) {
       return;
     }
-  
+
     this.handleDailyUpdate();
   }
 
@@ -36,37 +36,38 @@ export default class StoryAggregatorPlugin extends Plugin {
     }
   }
 
-  extractStory(noteContent) {
+  extractStory(noteContent: string) {
     const match = noteContent.match(/## 5 minute story\n([^#]+)/);
     return match ? match[1].trim() : null;
   }
 
-  async updateAggregateNote(date, story) {
+  async updateAggregateNote(date: string, story: string) {
     const aggregateNotePath = 'All 5 Minute Stories.md';
     let aggregateNote = this.app.vault.getAbstractFileByPath(aggregateNotePath);
-
     if (!(aggregateNote instanceof TFile)) {
       aggregateNote = await this.app.vault.create(aggregateNotePath, ''); // Create new note if doesn't exist
     }
 
-    let aggregateContent = await this.app.vault.read(aggregateNote);
-    const storyIndex = aggregateContent.indexOf(`| [${date}](${date}) |`);
+    if (aggregateNote instanceof TFile) {
+      let aggregateContent = await this.app.vault.read(aggregateNote);
+      const storyIndex = aggregateContent.indexOf(`| [${date}](${date}) |`);
 
-    if (storyIndex !== -1) {
-      const endOfStoryIndex = aggregateContent.indexOf('\n', storyIndex);
-      if (endOfStoryIndex !== -1) {
-        aggregateContent = aggregateContent.slice(0, storyIndex) + aggregateContent.slice(endOfStoryIndex + 1);
-      } else {
-        aggregateContent = aggregateContent.slice(0, storyIndex);
+      if (storyIndex !== -1) {
+        const endOfStoryIndex = aggregateContent.indexOf('\n', storyIndex);
+        if (endOfStoryIndex !== -1) {
+          aggregateContent = aggregateContent.slice(0, storyIndex) + aggregateContent.slice(endOfStoryIndex + 1);
+        } else {
+          aggregateContent = aggregateContent.slice(0, storyIndex);
+        }
       }
-    }
 
-    if (aggregateContent !== '' && !aggregateContent.endsWith('\n')) {
-      aggregateContent += '\n';
-    }
-    aggregateContent += `| [${date}](${date}) | ${story.replace(/\n/g, ' ')} |`;
+      if (aggregateContent !== '' && !aggregateContent.endsWith('\n')) {
+        aggregateContent += '\n';
+      }
+      aggregateContent += `| [${date}](${date}) | ${story.replace(/\n/g, ' ')} |`;
 
-    await this.app.vault.modify(aggregateNote, aggregateContent);
+      await this.app.vault.modify(aggregateNote, aggregateContent);
+    }
   }
 }
 
